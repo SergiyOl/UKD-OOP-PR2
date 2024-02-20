4bd551b7021a82b84de5572bee563ce7b4d2093d
SergiyOl <ser4002@gmail.com>
2024-02-20 10:45:52 +0200

    Add project files.

---
+++ UKD-OOP-PR2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UKD_OOP_PR2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
