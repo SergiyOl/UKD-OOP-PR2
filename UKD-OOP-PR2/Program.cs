2a6c06a29be0a15b80526f648f7c92ba210ad886
SergiyOl <ser4002@gmail.com>
2024-03-02 11:05:42 +0200

    Formatted questionnare creation

--- UKD-OOP-PR2/Program.cs
+++ UKD-OOP-PR2/Program.cs
@@ -9,45 +9,39 @@ namespace UKD_OOP_PR2
         {
             //Створення бази даних
             PersonDataBase dataBase = new();
-            //Запис мов
-            List<Language> olegLang = new();
-            olegLang.Add(new Language("English", "B1", "in progres"));
-            List<Language> joshLang = new();
-            joshLang.Add(new Language("Ukrainian", "C2", "done"));
-            joshLang.Add(new Language("English", "B2", "in progres"));
-            List<Language> vasilLang = new();
-            vasilLang.Add(new Language("English", "B1", "in progres"));
-            vasilLang.Add(new Language("Spanish", "A2", "paused"));
-            //Запис хоббі
-            List<Hobby> olegHobby = new();
-            olegHobby.Add(new Hobby("Learning english", 30, 5));
-            List<Hobby> joshHobby = new();
-            joshHobby.Add(new Hobby("Playing games", 60, 1));
-            joshHobby.Add(new Hobby("Stydying IT", 120, 5));
-            List<Hobby> vasilHobby = new();
-            vasilHobby.Add(new Hobby("Reading books", 45, 2));
-            vasilHobby.Add(new Hobby("Learning spanish", 60, 2));
-            //Запис пошт
-            List<string> olegEmails = new();
-            List<string> joshEmails = new();
-            List<string> vasilEmails = new();
-            //Запис телефонів
-            List<Phone> olegPhones = new();
-            olegPhones.Add(new Phone("+0999673914", "Vodaphone", 140));
-            List<Phone> joshPhones = new();
-            joshPhones.Add(new Phone("+0996782364", "Vodaphone", 120));
-            List<Phone> vasilPhones = new();
-            vasilPhones.Add(new Phone("+0651396513", "Kyivstar", 150));
+
             //Створення анкет
-            dataBase.AddPerson(new Person("Oleg", olegLang, olegHobby, new Contacts(olegEmails, olegPhones)));
-            dataBase.AddPerson(new Person("Josh", joshLang, joshHobby, new Contacts(joshEmails, joshPhones)));
-            dataBase.AddPerson(new Person("Vasil", vasilLang, vasilHobby, new Contacts(vasilEmails, vasilPhones)));
+            dataBase.AddPerson(new Person("Oleg", //Ім'я
+                                          new List<Language> {new Language("English", "B1", "in progres")}, //Мови
+                                          new List<Hobby> {new Hobby("Learning english", 30, 5)}, //Хоббі
+                                          new Contacts(new List<string> {"olegmail@gmail.com"}, //Пошти
+                                                       new List<Phone> {new Phone("+0999673914", "Vodaphone", 140), 
+                                                                        new Phone("+0659283091", "Kyivstar", 150)}) //Телефони
+                                          ));
+            dataBase.AddPerson(new Person("Josh", //Ім'я
+                                          new List<Language> {new Language("Ukrainian", "C2", "done"),
+                                                              new Language("English", "B2", "in progres")}, //Мови
+                                          new List<Hobby> {new Hobby("Playing games", 60, 1), 
+                                                           new Hobby("Stydying IT", 120, 5)}, //Хоббі
+                                          new Contacts(new List<string> { "joshmail@gmail.com" }, //Пошти
+                                                       new List<Phone> { new Phone("+0996782364", "Vodaphone", 120) }) //Телефони
+                                          ));
+            dataBase.AddPerson(new Person("Vasil", //Ім'я
+                                          new List<Language> {new Language("English", "B1", "in progres"), 
+                                                              new Language("Spanish", "A2", "paused")}, //Мови
+                                          new List<Hobby> { new Hobby("Reading books", 45, 2), 
+                                                            new Hobby("Learning spanish", 60, 2)}, //Хоббі
+                                          new Contacts(new List<string> { "vasilmail@gmail.com" }, //Пошти
+                                                       new List<Phone> { new Phone("+0651396513", "Kyivstar", 145) }) //Телефони
+                                          ));
+
             //Запис фінансів
             dataBase.allPerson.Find(x => x.name == "Oleg").financialInfo.addFinancialArticle("Programing courses", 1200, new DateTime(2024, 3, 4));
             dataBase.allPerson.Find(x => x.name == "Oleg").financialInfo.addFinancialArticle("Games", 270.40, new DateTime(2024, 3, 15));
             dataBase.allPerson.Find(x => x.name == "Oleg").financialInfo.addFinancialArticle("English courses", 1200, new DateTime(2024, 4, 1));
             dataBase.allPerson.Find(x => x.name == "Oleg").financialInfo.addFinancialArticle("Bus ticket", 65, new DateTime(2024, 4, 5));
-            //Виведення в консоль
+
+            //Тестування
             foreach (Person item in dataBase.allPerson)
             {
                 Console.WriteLine(item.name);
