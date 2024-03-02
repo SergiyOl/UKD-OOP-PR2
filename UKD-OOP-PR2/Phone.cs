b4dd56145369f6daecef989e9c4081de93f501a4
SergiyOl <ser4002@gmail.com>
2024-03-02 12:39:08 +0200

    Created testing section

--- UKD-OOP-PR2/Phone.cs
+++ UKD-OOP-PR2/Phone.cs
@@ -8,13 +8,13 @@ namespace UKD_OOP_PR2
 {
     public class Phone
     {
-        public string emailAdress;
+        public string number;
         public string mobileOperator;
         public int costPerMonth;
 
-        public Phone(string _emailAdress, string _mobileOperator, int _costPerMonth)
+        public Phone(string _number, string _mobileOperator, int _costPerMonth)
         {
-            emailAdress = _emailAdress;
+            number = _number;
             mobileOperator = _mobileOperator;
             costPerMonth = _costPerMonth;
         }
--- UKD-OOP-PR2/Program.cs
+++ UKD-OOP-PR2/Program.cs
@@ -12,8 +12,10 @@ namespace UKD_OOP_PR2
 
             //Створення анкет
             dataBase.AddPerson(new Person("Oleg", //Ім'я
-                                          new List<Language> {new Language("English", "B1", "in progres")}, //Мови
-                                          new List<Hobby> {new Hobby("Learning english", 30, 5)}, //Хоббі
+                                          new List<Language> {new Language("Ukrainian", "C2", "done"), 
+                                                              new Language("English", "B1", "in progres")}, //Мови
+                                          new List<Hobby> {new Hobby("Learning english", 60, 5), 
+                                                           new Hobby("Playing board games", 40, 1)}, //Хоббі
                                           new Contacts(new List<string> {"olegmail@gmail.com"}, //Пошти
                                                        new List<Phone> {new Phone("+0999673914", "Vodaphone", 140), 
                                                                         new Phone("+0659283091", "Kyivstar", 150)}) //Телефони
@@ -42,23 +44,100 @@ namespace UKD_OOP_PR2
             dataBase.allPerson.Find(x => x.name == "Oleg").financialInfo.addFinancialArticle("Bus ticket", 65, new DateTime(2024, 4, 5));
 
             //Тестування
+            //Показ всіх анкет
+            Console.WriteLine("Всі анкети");
             foreach (Person item in dataBase.allPerson)
             {
                 Console.WriteLine(item.name);
             }
+            //Показ повної анкети
+            Console.WriteLine("");
+            Console.WriteLine("Анкета студента Oleg");
+            Person questionnare = dataBase.allPerson.Find(x => x.name == "Oleg");
+            Console.WriteLine($"Ім'я студента: {questionnare.name}");
+            Console.WriteLine($"Дата створення анкети: {questionnare.creationDate}");
+            Console.WriteLine("Вивчені мови:");
+            foreach (var item in questionnare.languages)
+            {
+                Console.WriteLine($"Мова: {item.lang}");
+                Console.WriteLine($"Рівень: {item.level}");
+                Console.WriteLine($"Статус: {item.status}");
+            }
+            Console.WriteLine("Всі захоплення:");
+            foreach (var item in questionnare.hobbies)
+            {
+                Console.WriteLine($"Захоплення: {item.activity}");
+                Console.WriteLine($"Затрачений час (хв): {item.timeTaken}");
+                Console.WriteLine($"Цінність для IT (від 1 до 5): {item.usefullnessForIT}");
+            }
+            Console.WriteLine("Контактна інформація:");
+            Console.WriteLine("Електронні пошти:");
+            foreach (var item in questionnare.contactInfo.emailAdresses)
+            {
+                Console.WriteLine($"{item}");
+            }
+            Console.WriteLine("Номери телефонів:");
+            foreach (var item in questionnare.contactInfo.phoneNumbers)
+            {
+                Console.WriteLine($"Номер: {item.number}");
+                Console.WriteLine($"Мобільний оператор: {item.mobileOperator}");
+                Console.WriteLine($"Плата за місяць (грн): {item.costPerMonth}");
+            }
+            Console.WriteLine("Фінансова інформація:");
+            Console.WriteLine("Всі виттрати:");
+            foreach (var item in questionnare.financialInfo.financialArticles)
+            {
+                Console.WriteLine($"Назва: {item.name}");
+                Console.WriteLine($"Використані кошти (грн): {item.cost}");
+                Console.WriteLine($"Дата витрати: {item.creationDate}");
+            }
+            //Тестування методів
+            Console.WriteLine("");
+            Console.WriteLine("Тестування методів");
+
+            Console.WriteLine("AllUsedLanguages:");
             foreach (var lang in dataBase.AllUsedLanguages())
             {
                 Console.WriteLine(lang);
             }
-            Console.WriteLine(dataBase.SpecificLanguageUsage("English"));
-            Console.WriteLine(dataBase.SpecificLanguageLevelUsage("English", "B1"));
-            /*Console.WriteLine(dataBase.allPerson.Find(x => x.name == "Oleg").contactInfo.MinMaxPhoneCost(true));*/
 
+            Console.WriteLine("SpecificLanguageUsage (English):");
+            Console.WriteLine(dataBase.SpecificLanguageUsage("English"));
+            Console.WriteLine("SpecificLanguageUsage (Japanese):");
+            Console.WriteLine(dataBase.SpecificLanguageUsage("Japanese"));
+
+            Console.WriteLine("SpecificLanguageLevelUsage (English, B1):");
+            Console.WriteLine(dataBase.SpecificLanguageLevelUsage("English", "B1"));
+            Console.WriteLine("SpecificLanguageLevelUsage (Japanese, B1):");
+            Console.WriteLine(dataBase.SpecificLanguageLevelUsage("Japanese", "B1"));
+
+            Console.WriteLine("PersonPhoneCost (Oleg) (isMax = true):");
+            Console.WriteLine(dataBase.allPerson.Find(x => x.name == "Oleg").contactInfo.MinMaxPhoneCost(true));
+            Console.WriteLine("PersonPhoneCost (Oleg) (isMax = false):");
+            Console.WriteLine(dataBase.allPerson.Find(x => x.name == "Oleg").contactInfo.MinMaxPhoneCost(false));
+
+            Console.WriteLine("MinMaxMultiplePersonPhoneCost (2, true):");
             foreach (var item in dataBase.MinMaxMultiplePersonPhoneCost(2, true))
             {
                 Console.WriteLine(item.name);
             }
+            Console.WriteLine("MinMaxMultiplePersonPhoneCost (2, false):");
+            foreach (var item in dataBase.MinMaxMultiplePersonPhoneCost(2, false))
+            {
+                Console.WriteLine(item.name);
+            }
+            Console.WriteLine("MinMaxMultiplePersonPhoneCost (0, true):");
+            foreach (var item in dataBase.MinMaxMultiplePersonPhoneCost(0, true))
+            {
+                Console.WriteLine(item.name);
+            }
+
+            Console.WriteLine("CalculateSpentMoneyPerMonth (3, 2024):");
             Console.WriteLine(dataBase.allPerson.Find(x => x.name == "Oleg").financialInfo.CalculateSpentMoneyPerMonth(3, 2024));
+            Console.WriteLine("CalculateSpentMoneyPerMonth (4, 2024):");
+            Console.WriteLine(dataBase.allPerson.Find(x => x.name == "Oleg").financialInfo.CalculateSpentMoneyPerMonth(4, 2024));
+            Console.WriteLine("CalculateSpentMoneyPerMonth (5, 2024):");
+            Console.WriteLine(dataBase.allPerson.Find(x => x.name == "Oleg").financialInfo.CalculateSpentMoneyPerMonth(5, 2024));
 
             Console.ReadLine();
         }
